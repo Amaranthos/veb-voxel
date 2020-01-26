41064439643427b87de6f26913b562a808cc791f
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2020-01-26 22:52:38 +1000

    Draw many rectangles

--- src/index.js
+++ src/index.js
@@ -8,6 +8,25 @@ const resizeCanvas = canvas => {
   }
 };
 
+const randomInt = range => Math.floor(Math.random() * range);
+
+const setRectangle = (gl, x, y, width, height) => {
+  const x1 = x;
+  const x2 = x + width;
+  const y1 = y;
+  const y2 = y + height;
+
+  // prettier-ignore
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    x1, y1,
+    x2, y1,
+    x1, y2,
+    x1, y2,
+    x2, y1,
+    x2, y2
+  ]), gl.STATIC_DRAW);
+};
+
 // main
 (() => {
   const canvas = document.getElementById("render");
@@ -25,7 +44,7 @@ const resizeCanvas = canvas => {
 
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  const positions = [0, 0, 0, 0.5, 0.7, 0];
+  const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   const vao = gl.createVertexArray();
@@ -33,14 +52,12 @@ const resizeCanvas = canvas => {
 
   gl.enableVertexAttribArray(positionAttributeLocation);
 
-  const attributes = {
-    size: 2,
-    type: gl.FLOAT,
-    normalize: false,
-    stride: 0,
-    offset: 0
-  };
   gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+  const resolutionUniformLocation = gl.getUniformLocation(
+    program,
+    "u_resolution"
+  );
 
   resizeCanvas(gl.canvas);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -48,7 +65,29 @@ const resizeCanvas = canvas => {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.useProgram(program);
+
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
   gl.bindVertexArray(vao);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  var colourLocation = gl.getUniformLocation(program, "u_colour");
+
+  for (const x of Array(50).keys()) {
+    setRectangle(
+      gl,
+      randomInt(gl.canvas.width),
+      randomInt(gl.canvas.height),
+      randomInt(gl.canvas.width),
+      randomInt(gl.canvas.height)
+    );
+
+    gl.uniform4f(
+      colourLocation,
+      Math.random(),
+      Math.random(),
+      Math.random(),
+      1
+    );
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
 })();
--- src/shaders/source.fragment.js
+++ src/shaders/source.fragment.js
@@ -1,9 +1,11 @@
 export const fragment = `# version 300 es
 precision mediump float;
 
+uniform vec4 u_colour;
+
 out vec4 outColour;
 
 void main() {
-	outColour = vec4(1, 0, 0.5, 1);
+	outColour = u_colour;
 }
 `;
