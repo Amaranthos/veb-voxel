8b46741020b9d271c09b0b7a24d416ccc1a9f129
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2020-01-26 23:29:46 +1000

    Translate rectangle

--- src/index.js
+++ src/index.js
@@ -27,7 +27,42 @@ const setRectangle = (gl, x, y, width, height) => {
   ]), gl.STATIC_DRAW);
 };
 
+const drawScene = (
+  gl,
+  program,
+  vao,
+  positionBuffer,
+  { resolutionUniformLocation, colourLocation, translationUniformLocation },
+  { translation, width, height, colour }
+) => {
+  resizeCanvas(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(program);
+
+  gl.bindVertexArray(vao);
+
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  setRectangle(gl, 0, 0, width, height);
+
+  gl.uniform4fv(colourLocation, colour);
+
+  gl.uniform2fv(translationUniformLocation, translation);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+};
+
 // main
+
+let translation = [0, 0];
+const width = 100;
+const height = 30;
+const colour = [Math.random(), Math.random(), Math.random(), 1];
+
 (() => {
   const canvas = document.getElementById("render");
   const gl = canvas.getContext("webgl2");
@@ -40,54 +75,44 @@ const setRectangle = (gl, x, y, width, height) => {
   const program = createProgram(gl, vertexShader, fragmentShader);
 
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  const positionBuffer = gl.createBuffer();
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  const vao = gl.createVertexArray();
-  gl.bindVertexArray(vao);
-
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
+  const colourLocation = gl.getUniformLocation(program, "u_colour");
   const resolutionUniformLocation = gl.getUniformLocation(
     program,
     "u_resolution"
   );
+  const translationUniformLocation = gl.getUniformLocation(
+    program,
+    "u_translation"
+  );
 
-  resizeCanvas(gl.canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  const positionBuffer = gl.createBuffer();
+  const vao = gl.createVertexArray();
 
-  gl.useProgram(program);
-
-  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
   gl.bindVertexArray(vao);
 
-  var colourLocation = gl.getUniformLocation(program, "u_colour");
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  for (const x of Array(50).keys()) {
-    setRectangle(
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+  setInterval(() => {
+    let [x, y] = translation;
+
+    x += 1;
+    y += 1;
+
+    if (x >= gl.canvas.width) x = 0;
+    if (y >= gl.canvas.height) y = 0;
+
+    translation = [x, y];
+
+    drawScene(
       gl,
-      randomInt(gl.canvas.width),
-      randomInt(gl.canvas.height),
-      randomInt(gl.canvas.width),
-      randomInt(gl.canvas.height)
+      program,
+      vao,
+      positionBuffer,
+      { translationUniformLocation, resolutionUniformLocation, colourLocation },
+      { translation, width, height, colour }
     );
-
-    gl.uniform4f(
-      colourLocation,
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      1
-    );
-
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-  }
+  }, 10);
 })();
